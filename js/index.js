ade002c62eae0d55ecb66f0c6fec224ac3312199
Andela Stevanovic <andjela.s166@gmail.com>
2022-01-04 16:03:30 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -10,26 +10,35 @@ const init = () => {
         fetch(`https://api.github.com/search/users?q=${input.value}`)     
         .then(response => response.json())
         .then(data => { 
-            // console.log(data)
+            console.log(data)
             addUserDataToDom(data)
             })
-
-               //using the result of the search, display info avout the user 
-           //username, avatar_url and link to their profile
-
- 
         })
     }
-  
+        //using the result of the search, display info avout the user 
+        //username, avatar_url and link to their profile
     function addUserDataToDom(data) {
         const li = document.createElement('li')
         const userList = document.getElementById('user-list')
         const userName = data.items[0].login
-        const url = data.items[0].url
+        const id = data.items[0].id
         const avatarUrl = data.items[0].avatar_url
-       li.innerHTML = userName
-    
+        
+        //create p element to add your user data
+        const pUserName = document.createElement("p")
+        const pId = document.createElement("p")
+        const pAvatarUrl = document.createElement("p")
+
+        pUserName.innerText = userName 
+        pId.innerText = id
+        pAvatarUrl.innerText = avatarUrl
+
+        //append to the page
+       li.appendChild(pUserName)
+       li.appendChild(pId)
+       li.appendChild(pAvatarUrl)
         userList.appendChild(li)
+       
     }
 
 document.addEventListener('DOMContentLoaded', init);
